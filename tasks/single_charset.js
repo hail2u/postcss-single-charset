cae933b0e3ca9fd12b812b027069f2f04020e48d
Kyo Nagashima <hail2u@gmail.com>
2015-01-12 16:38:43 +0900

    Ditch `grunt.file` methods with fs-extra module

--- tasks/single_charset.js
+++ tasks/single_charset.js
@@ -5,6 +5,7 @@ module.exports = function (grunt) {
   pkg.name = pkg.name.replace(/^postcss-/, '').replace(/-/g, '_');
 
   grunt.registerMultiTask(pkg.name, pkg.description, function () {
+    var fs = require('fs-extra');
     var postcss = require('postcss');
 
     var options = this.options({});
@@ -17,7 +18,7 @@ module.exports = function (grunt) {
       var src = file.src[0];
       var dest = file.dest;
 
-      if (!grunt.file.exists(src)) {
+      if (!fs.existsSync(src)) {
         grunt.log.warn('Source file "' + src + '" not found.');
 
         return;
@@ -31,14 +32,14 @@ module.exports = function (grunt) {
       var processed = postcss().use(
         require('../index')()
       ).process(
-        grunt.file.read(src), options
+        fs.readFileSync(src, 'utf8'), options
       );
-      grunt.file.write(dest, processed.css);
+      fs.outputFileSync(dest, processed.css);
       grunt.log.writeln('File "' + dest + '" created.');
 
       if (processed.map) {
         var map = dest + '.map';
-        grunt.file.write(map, processed.map);
+        fs.outputFileSync(map, processed.map);
         grunt.log.writeln('File "' + map + '" created.');
       }
     });
