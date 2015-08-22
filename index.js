a5732142f47ab920a664e7a35a10ddfe314c647a
Kyo Nagashima <hail2u@gmail.com>
2015-08-22 20:47:43 +0900

    Update PostCSS to v5
    This closes #4.

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var postcss = require("postcss");
 module.exports = postcss.plugin("single-charset", function () {
   return function (css) {
     var metCharset = false;
-    css.eachAtRule("charset", function (atRule) {
+    css.walkAtRules("charset", function (atRule) {
       if (!metCharset) {
         metCharset = true;
         atRule.parent.prepend(atRule.clone());
       }
 
-      atRule.removeSelf();
+      atRule.remove();
     });
   };
 });
